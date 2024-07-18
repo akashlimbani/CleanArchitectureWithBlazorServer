247d12722eabb376757fbb18164b6ca450acc6a9
Akash <lakash2710@gmail.com>
2024-07-18 20:41:45 +0530

    Add the timezoneoffset dropdown in the tenant page -#task

--- src/Application/Features/Tenants/Queries/Pagination/TenantsPaginationQuery.cs
+++ src/Application/Features/Tenants/Queries/Pagination/TenantsPaginationQuery.cs
@@ -1,4 +1,4 @@
-// Licensed to the .NET Foundation under one or more agreements.
+﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
 using CleanArchitecture.Blazor.Application.Features.Tenants.Caching;
@@ -8,6 +8,7 @@ namespace CleanArchitecture.Blazor.Application.Features.Tenants.Queries.Paginati
 
 public class TenantsWithPaginationQuery : PaginationFilter, ICacheableRequest<PaginatedData<TenantDto>>
 {
+    public decimal TimeZoneOffset { get; set; }
     public TenantsPaginationSpecification Specification => new(this);
     public string CacheKey => TenantCacheKey.GetPaginationCacheKey($"{this}");
     public MemoryCacheEntryOptions? Options => TenantCacheKey.MemoryCacheEntryOptions;
